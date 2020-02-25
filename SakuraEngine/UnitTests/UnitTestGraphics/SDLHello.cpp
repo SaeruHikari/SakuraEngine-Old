64cdb9ba0dafbd61731b623f4a7128bce23ba0c3
SaeruHikari <39457738+misakaXunyan@users.noreply.github.com>
2020-02-25 23:56:22 +0800

    Add SDL2 puller and FindVk

---
+++ SakuraEngine/UnitTests/UnitTestGraphics/SDLHello.cpp
@@ -0,0 +1,38 @@
+/*
+ * @This File is Part of Sakura by SaeruHikari: 
+ * @Description: Copyright SaeruHikari
+ * @Version: 0.1.0
+ * @Autor: SaeruHikari
+ * @Date: 2020-02-25 22:43:56
+ * @LastEditors: SaeruHikari
+ * @LastEditTime: 2020-02-25 23:43:50
+ */
+#include <iostream>
+extern "C"
+{
+	#include "SDL.h"
+	#include "SDL_surface.h"
+	#undef main
+}
+
+using namespace std;
+int main(int argc, char** argv)
+{
+	SDL_Window *win = SDL_CreateWindow("hello world!", 100, 100, 640, 480, SDL_WINDOW_RESIZABLE);
+	SDL_Surface *picture = SDL_LoadBMP("E:/material/lessons.bmp");
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, picture);
+ 
+	SDL_FreeSurface(picture);
+	SDL_RenderClear(ren);
+	SDL_RenderCopy(ren, tex, NULL, NULL);
+	SDL_RenderPresent(ren);
+	SDL_Delay(4000);
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+ 
+	cout << "Success!" << endl;
+	return 0;
+}
--- SakuraEngine/UnitTests/UnitTestGraphics/VkTriangle.cpp
+++ SakuraEngine/UnitTests/UnitTestGraphics/VkTriangle.cpp
@@ -427,8 +427,8 @@ private:
     }
 
     void createGraphicsPipeline() {
-        auto vertShaderCode = readFile("shaders/vert.spv");
-        auto fragShaderCode = readFile("shaders/frag.spv");
+        auto vertShaderCode = readFile("SakuraEngine/UnitTests/UnitTestGraphics/vert.spv");
+        auto fragShaderCode = readFile("SakuraEngine/UnitTests/UnitTestGraphics/frag.spv");
 
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
