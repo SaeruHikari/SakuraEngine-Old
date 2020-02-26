3a0dd20947880c73c3dc4bb6471ef1754cda753d
SaeruHikari <3045349520@qq.com>
2020-02-26 21:35:23 +0800

    Built on linux and remove generated files

--- SakuraEngine/SPAModules.generated.h
+++
@@ -1,5 +0,0 @@
-//A header file genereate by Sakura Module tool
-//Contains the header of static modules of Sakura Engine
-//With the MIT License Copyright!
-#pragma once
-#include "D:/Coding/SakuraEngine/SakuraEngine\Core/CoreModule.h"
