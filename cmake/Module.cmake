c91e46f84bf89288e393a1bc4feec3fb0a7d8504
SaeruHikari <3045349520@qq.com>
2020-12-13 17:32:48 +0800

    给VS项目加上美美的小Folder

--- cmake/Module.cmake
+++ cmake/Module.cmake
@@ -28,6 +28,24 @@ function(GlobCXXFiles AbsPath OutFiles)
     set(${OutFiles} ${GlobFiles} PARENT_SCOPE)
 endfunction()
 
+function(SourceGroupByDir)
+    CMAKE_PARSE_ARGUMENTS(  
+        m "" "cur_dir"  
+        "src_files"  
+        ${ARGN}  
+    )
+    foreach(sgbd_file ${m_src_files})
+        string(REGEX REPLACE ${m_cur_dir}/\(.*\) \\1 sgbd_fpath ${sgbd_file})
+        string(REGEX REPLACE "\(.*\)/.*" \\1 sgbd_group_name ${sgbd_fpath})
+        string(COMPARE EQUAL ${sgbd_fpath} ${sgbd_group_name} sgbd_nogroup)
+        string(REPLACE "/" "\\" sgbd_group_name ${sgbd_group_name})
+        if(sgbd_nogroup)
+            set(sgbd_group_name "\\")
+        endif(sgbd_nogroup)
+        source_group(${sgbd_group_name} FILES ${sgbd_file})
+    endforeach(sgbd_file)
+endfunction(SourceGroupByDir)
+
 
 
 function(Module)
@@ -68,7 +86,6 @@ function(Module)
         set(MODULE_SRC ${MODULE_SRC} ${MODULE_SRC_T})
     endforeach()
 
-
     if(${MODULE_TYPE} STREQUAL "Library")
         add_library(${MODULE_NAME} SHARED ${MODULE_SRC})
         target_compile_definitions(${MODULE_NAME} 
@@ -85,7 +102,6 @@ function(Module)
         add_executable(SakuraEngine::${MODULE_NAME} ALIAS ${MODULE_NAME})
     endif(${MODULE_TYPE} STREQUAL "Library")
 
-    #target_sources(${MODULE_NAME} PRIVATE ${MODULE_SRC})
     target_include_directories(${MODULE_NAME}
         PUBLIC
         $<BUILD_INTERFACE:${MODULE_ABS_HEADER_DIR}>
@@ -124,6 +140,18 @@ function(Module)
         LIBRARY DESTINATION "/Lib"
     )
 
+    if(MSVC)
+        # Enable MSVC UnityBuild
+        set_target_properties(${MODULE_NAME} PROPERTIES VS_GLOBAL_EnableUnitySupport true)
+        SourceGroupByDir(
+            cur_dir
+            ${ENGINE_DIR}/Source/${CurrentScope}/${ProjectId}
+            src_files
+            ${MODULE_SRC}
+        )
+        set_property(TARGET "${MODULE_NAME}" PROPERTY FOLDER "${CurrentScope}/${ProjectId}")
+    endif(MSVC)
+
     set_property(GLOBAL PROPERTY ${CurrentScope}HasModule TRUE)
 endfunction(Module)
 
@@ -148,4 +176,8 @@ function(CMakeImportedModule)
         set_property(GLOBAL PROPERTY ${CurrentScope}HasCMakeImported TRUE)
     endif()
     add_library(CMakeImported::${MODULE_NAME} ALIAS ${MODULE_NAME})
+
+    if(MSVC)
+        set_property(TARGET "${MODULE_NAME}" PROPERTY FOLDER "${CurrentScope}/CMakeImported/${ProjectId}")
+    endif(MSVC)
 endfunction()
