205d3afd87f448bf67627b17232b0eb4c29f029a
BenzzZX <672619410@qq.com>
2020-12-15 01:24:27 +0800

    秘技，反复横跳

--- SakuraEngine/Source/Runtime/ECS/Include/ECS/ECS.h
+++ SakuraEngine/Source/Runtime/ECS/Include/ECS/ECS.h
@@ -24,7 +24,12 @@ namespace sakura::task_system::ecs
 		sakura::ecs::pass* create_pass(const sakura::ecs::filters& v, T paramList, gsl::span<core::codebase::shared_entry> sharedEntries = {})
 		{
 			pass_events.emplace_back(task_system::Event::Mode::Manual);
-			return static_cast<sakura::ecs::pass*>(base_t::create_pass(v, paramList, sharedEntries));
+			return base_t::create_pass(v, paramList, sharedEntries);
+		}
+		sakura::ecs::custom_pass* create_custom_pass(gsl::span<core::codebase::shared_entry> sharedEntries = {})
+		{
+			pass_events.emplace_back(task_system::Event::Mode::Manual);
+			return base_t::create_custom_pass(sharedEntries);
 		}
 		void wait()
 		{
@@ -34,6 +39,20 @@ namespace sakura::task_system::ecs
 		sakura::vector<task_system::Event> pass_events;
 		bool force_no_parallel = false;
 	};
+	template<class F>
+	FORCEINLINE task_system::Event schedule_custom(pipeline& pipeline, sakura::ecs::custom_pass& pass, F&& t, std::vector<task_system::Event> externalDependencies = {})
+	{
+		task_system::schedule([&, t]()
+		{
+			defer(pipeline.pass_events[pass.passIndex].signal());
+			forloop(i, 0, pass.dependencyCount)
+				pipeline.pass_events[pass.dependencies[i]->passIndex].wait();
+			for (auto& ed : externalDependencies)
+				ed.wait();
+			t();
+		});
+		return pipeline.pass_events[pass.passIndex];
+	}
 
 	template<bool ForceParallel = false, bool ForceNoParallel = false, class F>
 	FORCEINLINE task_system::Event schedule(
@@ -82,7 +101,6 @@ namespace sakura::task_system::ecs
 						t(pipeline, pass, tk);
 					});
 			}
-			delete& pass;
 		});
 		return pipeline.pass_events[pass.passIndex];
 	}
