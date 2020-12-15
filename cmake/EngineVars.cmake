f9bc5704a3f11c1a544e7f56fc86b858b2a523b6
SaeruHikari <3045349520@qq.com>
2020-12-16 01:03:47 +0800

    以奇怪的姿势支持RelWithDebInfo，你永远想不通CMake在想什么

--- cmake/EngineVars.cmake
+++ cmake/EngineVars.cmake
@@ -7,16 +7,15 @@ set(ENGINE_TOOLS_DIR ${ENGINE_DIR}/Binaries/Tools)
 set(FILE_SERVER_DIR "http://139.59.27.159//SaeruHikari")
 if(web)
 else()
-    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${ENGINE_BIN_DIR}/Debug)
-    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG ${ENGINE_BIN_DIR}/Debug)
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${ENGINE_BIN_DIR}/Debug)
-    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${ENGINE_BIN_DIR}/Release)
-    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE ${ENGINE_BIN_DIR}/Release)
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${ENGINE_BIN_DIR}/Release)
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Release>$<$<CONFIG:Release>:Debug>$<$<CONFIG:RelWithDebInfo>:Release>$<$<CONFIG:MinSizeRel>:Release>)
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Release>$<$<CONFIG:Release>:Debug>$<$<CONFIG:RelWithDebInfo>:Release>$<$<CONFIG:MinSizeRel>:Release>)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Release>$<$<CONFIG:Release>:Debug>$<$<CONFIG:RelWithDebInfo>:Release>$<$<CONFIG:MinSizeRel>:Release>)
 endif()
 
-link_directories(${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Release>$<$<CONFIG:Release>:Debug>)
-set(SAKURA_FINAL_BIN_DIR ${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>)
+link_directories(
+    ${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Release>$<$<CONFIG:Release>:Debug>$<$<CONFIG:RelWithDebInfo>:Release>$<$<CONFIG:MinSizeRel>:Release>)
+set(SAKURA_FINAL_BIN_DIR 
+    ${ENGINE_BIN_DIR}/$<$<CONFIG:Debug>:Debug>$<$<CONFIG:Release>:Release>$<$<CONFIG:RelWithDebInfo>:Release>$<$<CONFIG:MinSizeRel>:Release>)
 
 if(windows)
 find_package(Vulkan)
