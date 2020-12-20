a748d1b1786bff75439e1d14ee7958d1e6e4459f
SaeruHikari <3045349520@qq.com>
2020-12-21 02:02:35 +0800

    SRGB滚啊

--- SakuraEngine/Source/Runtime/RenderGraph/Source/Vulkan/SwapChainVulkan.cpp
+++ SakuraEngine/Source/Runtime/RenderGraph/Source/Vulkan/SwapChainVulkan.cpp
@@ -192,7 +192,7 @@ VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>
 {
 	for (const auto& availableFormat : availableFormats)
 	{
-		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+		if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_ADOBERGB_LINEAR_EXT)
 		{
 			return availableFormat;
 		}
