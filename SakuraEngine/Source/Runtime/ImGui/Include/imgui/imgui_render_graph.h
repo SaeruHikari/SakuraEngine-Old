3e74db00f2062ac448bbc8f158b7b02ff2021c68
SaeruHikari <3045349520@qq.com>
2020-12-21 23:46:07 +0800

    ImGui结构异动：保证本体的静态链接以确保性能

--- SakuraEngine/Source/Runtime/ImGui/Include/imgui/imgui_render_graph.h
+++ SakuraEngine/Source/Runtime/ImGui/Include/imgui/imgui_render_graph.h
@@ -2,9 +2,9 @@
 #include "RenderGraph/RenderGraph.h"
 #include "imgui/imgui.h"
 
-namespace sakura::graphics
+namespace sakura
 {
     ImGuiAPI void initialize_imgui(graphics::RenderGraph& render_graph, graphics::IRenderDevice& device);
 
-	ImGuiAPI void create_imgui_fonts(RenderCommandBuffer& buffer);
+    ImGuiAPI void create_imgui_fonts(graphics::RenderCommandBuffer& buffer);
 }
--- SakuraEngine/Source/Runtime/ImGui/Source/imgui_render_graph.cpp
+++ SakuraEngine/Source/Runtime/ImGui/Source/imgui_render_graph.cpp
@@ -5,27 +5,55 @@ using namespace sakura::graphics;
 
 static RenderPipelineHandle imgui_render_pipeline;
 
-ImGuiAPI void initialize_imgui(graphics::RenderGraph& render_graph, graphics::IRenderDevice& device)
+namespace sakura
 {
-    // Setup backend capabilities flags
-    ImGuiIO& io = ImGui::GetIO();
-    io.BackendRendererName = "imgui_render_graph";
-    // We can honor the ImDrawCmd::VtxOffset field, allowing for large meshes.
-    io.BackendFlags |= ImGuiBackendFlags_RendererHasVtxOffset;
-    // We can create multi-viewports on the Renderer side (optional)
-    io.BackendFlags |= ImGuiBackendFlags_RendererHasViewports;
 
-    // Upload Fonts
-}
+    void initialize_imgui(graphics::RenderGraph& render_graph, graphics::IRenderDevice& device)
+    {
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+        //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
+        //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
-ImGuiAPI void graphics::create_imgui_fonts(RenderCommandBuffer& buffer)
-{
-    ImGuiIO& io = ImGui::GetIO();
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+        ImGuiStyle& style = ImGui::GetStyle();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            style.WindowRounding = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
 
-    unsigned char* pixels;
-    int width, height;
-    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
-    size_t upload_size = width * height * 4 * sizeof(char);
+        io.BackendRendererName = "imgui_render_graph";
+        // We can honor the ImDrawCmd::VtxOffset field, allowing for large meshes.
+        io.BackendFlags |= ImGuiBackendFlags_RendererHasVtxOffset;
+        // We can create multi-viewports on the Renderer side (optional)
+        io.BackendFlags |= ImGuiBackendFlags_RendererHasViewports;
 
-	
+        // Upload Fonts
+        
+    }
+
+
+    void create_imgui_fonts(graphics::RenderCommandBuffer& buffer)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+
+        unsigned char* pixels;
+        int width, height;
+        io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+        size_t upload_size = width * height * 4 * sizeof(char);
+
+        
+
+    }
+
 }
